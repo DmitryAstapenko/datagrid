57aeecfc131dd16d3773ba5f16ee8d919810a567
DmitryAstapenko <dmitryastapenko1@gmail.com>
2020-03-10 15:31:51 +0300

    feat: add basic structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container></Container>
+  );
+}
+
+export default App;
