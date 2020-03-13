fe3a40d97413d92cdd03df16b5c22160ce848dcd
Dmitry Astapenko <dmitryastapenko1@gmail.com>
2020-03-13 18:13:20 +0300

    feat: add data for table

--- src/containers/grid/Grid.jsx
+++ src/containers/grid/Grid.jsx
@@ -4,6 +4,7 @@ import GridTitle from '../../components/gridTitle/GridTitle';
 import GridHeader from '../../components/gridHeader/GridHeader';
 import GridRow from '../../components/gridRow/GridRow';
 
+import data from '../../data.json'
 import './Grid.css';
 
 const COLUMNS = [
@@ -17,58 +18,7 @@ const COLUMNS = [
   { id: 8, name: "Location"},
 ];
 
-const CARS = [
-  {
-    id: 1,
-    make: 'Mercedes',
-    condition: true,
-    vehicleType: 'Saloon',
-    powerEngine: 235,    
-    price: { currency: '$', amount: 34000},
-    dateManufacture: '20-10-2019',
-    location: 'Berlin',
-  },
-  {
-    id: 2,
-    make: 'Mercedes',
-    condition: true,
-    vehicleType: 'Estate car',
-    powerEngine: 190,    
-    price: { currency: '$', amount: 55000},
-    dateManufacture: '22-01-2020',
-    location: 'Minsk',
-  },
-  {
-    id: 3,
-    make: 'BMW',
-    condition: false,
-    vehicleType: 'Coupe',
-    powerEngine: 420,    
-    price: { currency: '$', amount: 120000},
-    dateManufacture: '02-08-2018',
-    location: 'London',
-  },
-  {
-    id: 4,
-    make: 'Land Rover',
-    condition: true,
-    vehicleType: 'SUV',
-    powerEngine: 356,    
-    price: { currency: '$', amount: 157000},
-    dateManufacture: '13-11-2019',
-    location: 'Paris',
-  },
-  {
-    id: 5,
-    make: 'Mercedes',
-    condition: true,
-    vehicleType: 'Saloon',
-    powerEngine: 555,    
-    price: { currency: '$', amount: 235000},
-    dateManufacture: '24-11-2019',
-    location: 'Berlin',
-  },
-];
+const CARS = data.cars;
 
 class Grid extends Component {
   render() {
