9ae37aa8ca42eb2c486f646eb196e08d49e7c1f7
Dmitry Astapenko <dmitryastapenko1@gmail.com>
2020-03-17 20:24:18 +0300

    refactor: refactor code

--- src/store.js
+++ src/store.js
@@ -1,13 +1,11 @@
 import { createStore, compose } from 'redux';
 import rootReducer from './reducers/index';
 
-/* eslint-disable no-underscore-dangle */
-const composeEnhancers =
-  process.env.NODE_ENV !== 'production' &&
+const composeEnhancers = 
+  // process.env.NODE_ENV !== 'production' &&
   typeof window === 'object' &&
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
-/* eslint-enable */
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 
 const configureStore = preloadedState => (
   createStore(
